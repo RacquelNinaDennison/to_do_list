517cb0caf791677d7c5126074a35b3e60e1fe4da
RacquelNinaDennison <racquelnina@gmail.com>
2023-06-18 16:09:56 +0200

    Added the dynamic editing of notes

--- to_do_list/src/components/Note.jsx
+++ to_do_list/src/components/Note.jsx
@@ -1,16 +1,74 @@
-import React from "react";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import React, { useState } from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
 
 const Note = (props) => {
-	function handleClick() {
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedTitle, setEditedTitle] = useState(props.title);
+	const [editedContent, setEditedContent] = useState(props.content);
+
+	function handleDelete() {
 		props.deleteItem(props.id);
 	}
+
+	function handleEdit() {
+		setIsEditing(true);
+	}
+
+	function handleSave() {
+		// Perform the necessary actions to save the edited note
+		// For example, you can pass the editedTitle and editedContent to a function in the parent component to update the notes array.
+
+		// After saving, exit the edit mode
+		setIsEditing(false);
+	}
+
+	function handleCancel() {
+		// Revert the changes made during editing
+		setEditedTitle((prevState) => {
+			return prevState;
+		});
+		setEditedContent((prevState) => {
+			return prevState;
+		});
+
+		// Exit the edit mode
+		setIsEditing(false);
+	}
+
 	return (
 		<div className='note'>
-			<h1>{props.title}</h1>
-			<p>{props.content}</p>
-			<button onClick={handleClick}><DeleteOutlineIcon/></button>
+			{isEditing ? (
+				<>
+					<input
+						type='text'
+						value={editedTitle}
+						onChange={(e) => {
+							console.log(e.target.value);
+							setEditedTitle(e.target.value);
+						}}
+					/>
+					<textarea
+						value={editedContent}
+						onChange={(e) => setEditedContent(e.target.value)}
+					/>
+					<button onClick={handleSave}>Save</button>
+					<button onClick={handleCancel}>Cancel</button>
+				</>
+			) : (
+				<>
+					<h1>{editedTitle}</h1>
+					<p>{editedContent}</p>
+					<button onClick={handleEdit}>
+						<EditIcon />
+					</button>
+					<button onClick={handleDelete}>
+						<DeleteOutlineIcon />
+					</button>
+				</>
+			)}
 		</div>
 	);
 };
+
 export default Note;
